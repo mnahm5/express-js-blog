e0900e587c8ca13bd077acb8f5353a04eecd6329
Md Nadim Ahmed <ahmed.nadim59@gmail.com>
2016-12-06 22:39:21 +1100

    018 Categories - Basics

--- routes/categories.js
+++ routes/categories.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+Category = require('../models/category.js');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('categories', { title: 'Categories' });
+    Category.getCategory(function (err, categories) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('categories', {
+                title: 'Categories',
+                categories: categories
+            })
+        }
+    });
 });
 
 module.exports = router;
